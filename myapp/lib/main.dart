a6f359f6ede6398a9aaab1b3c2a71e087165bb01
niszco <nico.marszalek@gmail.com>
2023-05-28 21:07:20 +0200

    Ajouts: suppression de éléments de la liste, ajouts dans la liste

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -15,10 +15,12 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   late final TextEditingController controller;
+  late final List<int> deletedIndexes;
 
   @override
   void initState() {
     controller = TextEditingController();
+    deletedIndexes = [];
     super.initState();
   }
 
@@ -37,8 +39,10 @@ class _MyAppState extends State<MyApp> {
   ];
 
   void showMessageError(String message) {
-    ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(message), duration: const Duration(seconds: 2)));
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(message),
+      duration: const Duration(seconds: 2),
+    ));
   }
 
   void addPokemon() {
@@ -51,8 +55,28 @@ class _MyAppState extends State<MyApp> {
       showMessageError('Le nom ne peut pas être vide !');
       return;
     }
+
+    final lowercaseNameToAdd = nameToAdd.toLowerCase();
+    final trimmedNameToAdd = lowercaseNameToAdd.trim();
+
+    for (final pokemon in pokedex) {
+      final lowercasePokemonName = pokemon.name.toLowerCase();
+      final trimmedPokemonName = lowercasePokemonName.trim();
+
+      if (trimmedPokemonName == trimmedNameToAdd) {
+        showMessageError('Le Pokémon existe déjà dans la liste !');
+        return;
+      }
+    }
+
     setState(() {
       pokedex.add(Pokemon(icon: Icons.star, name: nameToAdd));
+    });
+  }
+
+  void removePokemon(int index) {
+    setState(() {
+      deletedIndexes.add(index);
     });
   }
 
@@ -68,46 +92,50 @@ class _MyAppState extends State<MyApp> {
         brightness: Brightness.dark,
       ),
       themeMode: ThemeMode.dark,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Première appli'),
-        ),
-        body: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              children: [
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Expanded(
-                      child: Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.white),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: TextField(
-                          controller: controller,
-                          decoration: InputDecoration(
-                            labelText: 'Ecrivez un nom',
-                            border: InputBorder.none,
+      home: ScaffoldMessenger(
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Première appli'),
+          ),
+          body: SingleChildScrollView(
+            child: Center(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.white),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: TextField(
+                            controller: controller,
+                            decoration: InputDecoration(
+                              labelText: 'Ecrivez un nom',
+                              border: InputBorder.none,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Flexible(
+                      Flexible(
                         child: IconButton(
-                            icon: const Icon(Icons.add),
-                            onPressed: () {
-                              addPokemon();
-                            })),
-                  ],
-                ),
-                for (final Pokemon item in pokedex)
-                  TheAmazingRow(
-                    icon: item.icon,
-                    label: item.name.toUpperCase(),
+                          icon: const Icon(Icons.add),
+                          onPressed: () => addPokemon(),
+                        ),
+                      ),
+                    ],
                   ),
-              ],
+                  for (int i = 0; i < pokedex.length; i++)
+                    if (!deletedIndexes.contains(i))
+                      TheAmazingRow(
+                        icon: pokedex[i].icon,
+                        label: pokedex[i].name.toUpperCase(),
+                        onDelete: () => removePokemon(i),
+                      ),
+                ],
+              ),
             ),
           ),
         ),
@@ -128,16 +156,16 @@ class TheAmazingRow extends StatefulWidget {
     Key? key,
     required this.icon,
     required this.label,
+    required this.onDelete,
   }) : super(key: key);
 
   final IconData icon;
   final String label;
+  final VoidCallback onDelete;
 
   @override
   State<TheAmazingRow> createState() => _TheAmazingRowState();
 }
-
-void deletePokemon(Pokemon pokemon) {}
 
 class _TheAmazingRowState extends State<TheAmazingRow> {
   @override
@@ -162,9 +190,7 @@ class _TheAmazingRowState extends State<TheAmazingRow> {
             ),
             IconButton(
               icon: const Icon(Icons.delete),
-              onPressed: () {
-                deletePokemon();
-              },
+              onPressed: widget.onDelete,
             ),
           ],
         ),
